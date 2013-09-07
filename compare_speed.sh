05c5365d65564f2e0bbf0dbb1393feb0e18cfada
Alexander Voigt <Alexander.Voigt@physik.tu-dresden.de>
2013-09-07 22:25:56 +0200

    check spectrum file and run SPheno

--- compare_speed.sh
+++ compare_speed.sh
@@ -14,6 +14,34 @@ measure_time() {
     time=$(echo "$_stop_time - $_start_time" | bc | awk '{printf "%f", $0}')
 }
 
+valid_spectrum() {
+    if test $# -ne 1 ; then
+        echo "valid_spectrum: only one argument accepted"
+        error="1"
+        return 1;
+    fi
+
+    _slha_file="$1"
+
+    if test "x${_slha_file}" = "x" ; then
+        error="1"
+        return 1;
+    fi
+    if ! test -r "${_slha_file}" ; then
+        error="1"
+        return 1;
+    fi
+
+    _is_valid=`awk -f valid_spectrum.awk ${_slha_file}`
+    if test "x$_is_validx" = "xyesx" ; then
+        error="0"
+        return 0; # ok
+    else
+        error="1"
+        return 1; # not ok
+    fi
+}
+
 # directory of this script
 BASEDIR=$(dirname $0)
 
@@ -56,7 +84,11 @@ if ! test -x $random_sign ; then
     exit 1
 fi
 
-echo "# m0/GeV | m12/GeV | tan(beta) | sign(mu) | A0/GeV | Softsusy time/s | Softsusy error"
+echo "# m0/GeV | m12/GeV | tan(beta) | sign(mu) | A0/GeV" \
+    " | Softsusy time/s | Softsusy error" \
+    " | FlexibleSUSY time/s | FlexibleSUSY error" \
+    " | SPheno time/s | SPheno error" \
+    " | SPhenoMSSM time/s | SPhenoMSSM error"
 
 while [ true ]
 do
@@ -66,6 +98,8 @@ do
     a0=`$random_float -1000 1000`
     tan_beta=`$random_float 1 100`
     sign_mu=`$random_sign`
+
+    rm -f out.spc SPheno.spc SPheno.spc.MSSM SPheno.out Messages.out
 
     slha_file="${BASEDIR}/${slha_template}.point"
 
@@ -78,9 +112,37 @@ do
          "   5   $a0"         \
         >> $slha_file
 
-    measure_time $ss_path leshouches < $slha_file > out 2>&1
+    measure_time $ss_path leshouches < $slha_file > out.spc 2>&1
     ss_time="$time"
+    if test "x$error" = "x0" ; then
+        valid_spectrum out.spc
+    fi
     ss_error="$error"
 
-    echo "$m0 \t $m12 \t $tan_beta \t $sign_mu \t $a0 \t $ss_time \t $ss_error"
+    measure_time $fs_path --slha-input-file=$slha_file --slha-output-file=out.spc > /dev/null 2>&1
+    fs_time="$time"
+    if test "x$error" = "x0" ; then
+        valid_spectrum out.spc
+    fi
+    fs_error="$error"
+
+    measure_time $sp_path $slha_file > /dev/null 2>&1
+    sp_time="$time"
+    if test "x$error" = "x0" ; then
+        valid_spectrum SPheno.spc
+    fi
+    sp_error="$error"
+
+    measure_time $spmssm_path $slha_file > /dev/null 2>&1
+    spmssm_time="$time"
+    if test "x$error" = "x0" ; then
+        valid_spectrum SPheno.spc.MSSM
+    fi
+    spmssm_error="$error"
+
+    echo "$m0 \t $m12 \t $tan_beta \t $sign_mu \t $a0" \
+        " \t $ss_time \t $ss_error" \
+        " \t $fs_time \t $fs_error" \
+        " \t $sp_time \t $sp_error" \
+        " \t $spmssm_time \t $spmssm_error"
 done
